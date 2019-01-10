dbdf7a6307cceeee5121afc6bfb4fc15419665bc
Junaid Azhar Shaikh <junaidazharshaikh@gmail.com>
2019-01-10 00:14:24 -0500

    added pipes to improve UI

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -19,9 +19,9 @@ export class EmployeesComponent implements OnInit {
   ngOnInit() {
     this.title = 'Employees';
     this.employees = [
-      { id: 1, name: 'Abd-Allah', city: 'Jeddah', salary: 6000, started: new Date(2017, 10, 10), projects: 2},
-      // { id: 2, name: 'Humed Shaikh', city: 'Nagpur', salary: 5000, started: new Date(2018, 4, 4), projects: 2},
-      // { id: 3, name: 'Mateen Athar', city: 'Nagpur', salary: 2500, started: new Date(2018, 11, 12), projects: 3},
+      { id: 1, name: 'Abd Allah', city: 'Jeddah', salary: 6000, started: new Date(2017, 10, 10), projects: 2},
+      { id: 2, name: 'Humed Shaikh', city: 'Nagpur', salary: 5000, started: new Date(2018, 4, 4), projects: 2},
+      { id: 3, name: 'Mateen Athar', city: 'Nagpur', salary: 2500, started: new Date(2018, 11, 12), projects: 3},
       { id: 4, name: 'Jeremy Tuner', city: 'New York', salary: 4000, started: new Date(2017, 10, 11), projects: 1},
       { id: 5, name: 'Goerge Washington', city: 'Rochester', salary: 5000, started: new Date(2016, 12, 11), projects: 1},
       { id: 6, name: 'Michael Bay', city: 'New Jersey', salary: 6000, started: new Date(2019, 1, 1), projects: 2},
--- src/app/employees/employees.module.ts
+++ src/app/employees/employees.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from './../shared/shared.module';
 import { EmployeesComponent } from './employees.component';
 import { EmployeesListComponent } from './employees-list/employees-list.component';
 import { EmployeesFilterTextboxComponent } from './employees-filter-textbox/employees-filter-textbox.component';
 
 @NgModule({
-  declarations: [EmployeesComponent, EmployeesListComponent, EmployeesFilterTextboxComponent],
-  imports: [CommonModule],
-  exports: [EmployeesComponent]
+  declarations: [ EmployeesComponent, EmployeesListComponent, EmployeesFilterTextboxComponent ],
+  imports: [ CommonModule, SharedModule ],
+  exports: [ EmployeesComponent ]
 })
 export class EmployeesModule { }
