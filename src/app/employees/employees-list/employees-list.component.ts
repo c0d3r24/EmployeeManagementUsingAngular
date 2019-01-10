b07562ecc9051f63e98bd78bd6fafad636a8e4cb
Junaid Azhar Shaikh <junaidazharshaikh@gmail.com>
2019-01-09 23:57:39 -0500

    add data binding

--- src/app/employees/employees-list/employees-list.component.ts
+++ src/app/employees/employees-list/employees-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { IEmployee } from './../../shared/interfaces';
 
 @Component({
@@ -8,12 +8,32 @@ import { IEmployee } from './../../shared/interfaces';
 })
 export class EmployeesListComponent implements OnInit {
 
+  private _employees: IEmployee[];
+  @Input() get employees(): IEmployee[] {
+    return this._employees;
+  }
+  set employees(value: IEmployee[]) {
+    if (value) {
+      this.filteredEmployees = this._employees = value;
+      this.calculateEmployeesProjects();
+    }
+  }
+
   filteredEmployees: IEmployee[] = [];
-  employeesSalaryTotal: number;
+  employeesProjectCount: number;
   currencyCode = 'USD';
   constructor() { }
 
   ngOnInit() {
+  }
+  calculateEmployeesProjects() {
+    this.employeesProjectCount = 0;
+    this.filteredEmployees.forEach((emp: IEmployee) => {
+      this.employeesProjectCount += emp.projects;
+    });
+  }
+
+  sortEmployee(field: string) {
 
   }
 
